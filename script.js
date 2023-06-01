c02dbfbcf7501c74fe369d013d389fbbcf1e53ff
Moa Svensson <moasvenssons97@gmail.com>
2023-06-01 16:11:02 +0200

    cleaned the code and looked over the indenting

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ fetchWeather('madrid');
 fetchWeather('oslo');
 fetchWeather('angers');
 
-
 function fetchWeather(cityName) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=168d9181df63606a05cb22cddcc68f85&units=metric`)
     .then((response) => response.json())
@@ -12,7 +11,6 @@ function fetchWeather(cityName) {
         document.getElementById(`${cityName}-pressure`).innerHTML = data.main.pressure + " hPa";
         document.getElementById(`${cityName}-wind`).innerHTML = data.wind.speed + " m/s";
         document.getElementById(`${cityName}-humidity`).innerHTML = data.main.humidity + " %";
-        console.log(data);
   
         // Call fetchWeather() again after 30 minutes
         setTimeout(fetchWeather, 1800000);
@@ -22,7 +20,6 @@ function fetchWeather(cityName) {
         alert("Something went wrong")
     });
 }
-
 
 let dateElement = document.getElementById("date");
 let timeElement = document.getElementById("time");
@@ -35,18 +32,17 @@ function formatTime (date) {
     return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
-
 function formatDate (date) {
     let DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let MONTH = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     return `${DAYS[date.getDay()]}, ${MONTH[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
-
 }
 
 //Updating every second
 setInterval(() => {
     let now = new Date();
+
     timeElement.textContent = formatTime(now);
     dateElement.textContent = formatDate(now);
 }, 1000);
